33d16bfe5c0ceb5067ef7661021adade4e16781d
Orhan Özkerçin <orhanozkercin@yahoo.com>
2021-04-23 13:44:16 +0300

    Axios add and finish project

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <span class="icon-wrapper" v-html="svg"></span>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import feather from 'feather-icons';
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+  computed: {
+    svg() {
+      return feather.icons['users'].toSvg({
+        class: 'icon',
+        width: this.width,
+        height: this.height,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+  margin-right: 6px;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+}
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
-    <h1>Event Listening</h1>
-    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
-      >Show Event #1</router-link
-    >
+    <h1>Event Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => {
+        this.events = res.data;
+      })
+      .catch((err) => console.error(err));
+  },
+};
+</script>
